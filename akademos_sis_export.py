2db5c9b19c74dc635a7727bb82b5ed16b39a5847
Michael Ghens <msghens@gmail.com>
2026-05-11 16:18:37 -0700

    Initial Commit

--- akademos_sis_export.py
+++ akademos_sis_export.py
@@ -1,10 +1,12 @@
 import sys
 import os
 from pprint import pprint
+from dotenv import load_dotenv
 
 # This sample uses a resource iterator to list all the academic periods
 
 import EllucianEthosPythonClient
+load_dotenv()
 
 ethosBaseURL = os.environ["ETHOSBASEURL"]
-ethosAppAPIKey = os.environ["ICETHOSDEVAPIKEY"]
+ethosAppAPIKey = os.environ["MSGETHOSDEVAPIKEY"]
